d33b2242c69c357abaea385a1a6ffde631847a1e
Jose Ernesto Carreno Bueno <josernesto1989@gmail.com>
2021-08-23 08:55:24 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import './card-left-image-right-label/CardLeftImageRightLabel'
+import CardLeftImageRightLabel from './card-left-image-right-label/CardLeftImageRightLabel';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CardLeftImageRightLabel></CardLeftImageRightLabel>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/card-left-image-right-label/CardLeftImageRightLabel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class CardLeftImageRightLabel extends React.Component {
+  render() {
+    return (
+    <div>
+          <div>Image</div>
+          <div>Value</div> 
+          </div>
+          );
+  }
+}
+
+export default CardLeftImageRightLabel;
